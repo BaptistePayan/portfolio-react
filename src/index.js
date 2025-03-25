e4c2c76da29b6aec0197a23f2e25a81a0d584e63
BaptistePayan <teck-bat@hotmail.fr>
2025-03-25 17:31:09 +0100

    ele

--- src/index.js
+++ src/index.js
@@ -16,20 +16,20 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <HashRouter>
+    <BrowserRouter>
     <App />
     <Footer />
-    </HashRouter>
+    </BrowserRouter>
     
   </React.StrictMode>
 );
 
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+)*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
